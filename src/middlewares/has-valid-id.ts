b63ddffd858aa47f65576092c65e2516ac8de05c
ENNA-Egor <e.yudakov@gmail.com>
2025-10-01 17:30:15 +0300

    Добавил валидацию отсутствия ID

--- src/middlewares/has-valid-id.ts
+++ src/middlewares/has-valid-id.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import { json } from "stream/consumers";
+import { BadRequestError } from "../errors/bad-request-error";
 
 export const hasValidId = (req: Request, res: Response, next: NextFunction) => {
     if (Number.isNaN(+req.params.id)){
-        res.status(400). json({mesage: 'Incorected Id'})
+        // res.status(400). json({mesage: 'Incorected Id'})
+         throw new BadRequestError ('Invalid ID')
     }
     next()
 }
